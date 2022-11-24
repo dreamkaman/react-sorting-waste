e3f757692599782912e8d8dc3324801ebae261b0
autudofe <ihor161994@gmail.com>
2022-11-24 19:42:40 +0200

    commit slider 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from '../src/modules/Header';
 import Footer from '../src/modules/Footer';
+import HeroSlider from './modules/HeroSlider';
 
 function App() {
   return (
     <div className="App">
       {/* <Header /> */}
       {/* <MyRoutes /> */}
+      <HeroSlider/>
       {/* <Footer /> */}
     </div>
   );
--- src/modules/HeroSlider/HeroSlider.jsx
+++ src/modules/HeroSlider/HeroSlider.jsx
@@ -1,5 +1,28 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import styles from "./HeroSlider.module.css";
+
 const HeroSlider = () => {
-  return <div>Hello HeroSlider!</div>;
+  return (
+    <Carousel autoPlay={true} infiniteLoop={true}>
+      <div>
+        {/*<img src="assets/1.jpeg" />*/}
+        <div className={styles.slide1}></div>
+        <p className="legend">Legend 1</p>
+      </div>
+      <div>
+        {/*<img src="assets/2.jpeg" />*/}
+        <div className="slide2"></div>
+        <p className="legend">Legend 2</p>
+      </div>
+      <div>
+        {/*<img src="assets/3.jpeg" />*/}
+        <div className="slide3"></div>
+        <p className="legend">Legend 3</p>
+      </div>
+    </Carousel>
+  );
 };
 
 export default HeroSlider;
