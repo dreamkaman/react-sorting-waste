e7530895f2c7848d9c243607671961173cb76c43
autudofe <ihor161994@gmail.com>
2022-11-25 19:48:26 +0200

    commit slider : slides and styles - prat 1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MyRoutes from 'Routes';
 
 
 import './App.css';
+import HeroSlider from './modules/HeroSlider';
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
       <Header />
 
       <MyRoutes />
+
+      <HeroSlider/>
 
       <Footer />
 
