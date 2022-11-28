bea72594e0133498e4e0e9ca551193f999d5194e
Andrii23LV <tretiak.andrii.lviv@gmail.com>
2022-11-28 12:33:18 +0200

    Responsive design for HelpPlanet and WebsiteUsersInfo modules added

--- src/modules/WebsiteUsersInfo/WebsiteUsersInfo.jsx
+++ src/modules/WebsiteUsersInfo/WebsiteUsersInfo.jsx
@@ -4,30 +4,30 @@ const WebsiteUsersInfo = () => {
   return (
     <section className={styles.container}>
       <div className={styles.statisticList}>
-        <div className={styles.statisticItem}>
-          <h2 className={styles.title}>30+</h2>
-          <p className={styles.category}>Customer</p>
-          <p className={styles.description}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus modi quibusdam</p>
+        <div className={styles.statisticList1}>
+          <div className={styles.statisticItem}>
+            <h2 className={styles.title}>30+</h2>
+            <p className={styles.category}>Customer</p>
+            <p className={styles.description}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus modi quibusdam</p>
+          </div>
+          <div className={styles.statisticItem}>
+            <h2 className={styles.title}>30+</h2>
+            <p className={styles.category}>Eco Services</p>
+            <p className={styles.description}>Lorem ipsum is simply dummy, adipisicing elit. Delectus modi quibusdam</p>
+          </div>
         </div>
-        <div className={styles.line}></div>
-        <div className={styles.statisticItem}>
-          <h2 className={styles.title}>30+</h2>
-          <p className={styles.category}>Eco Services</p>
-          <p className={styles.description}>Lorem ipsum is simply dummy, adipisicing elit. Delectus modi quibusdam</p>
-        </div>
-        <div className={styles.line}></div>
-        <div className={styles.statisticItem}>
-          <h2 className={styles.title}>30+</h2>
-          <p className={styles.category}>Waste Mangements</p>
-          <p className={styles.description}>Lorem ipsum is simply dummy, adipisicing elit. Delectus modi quibusdam</p>
-          <div className={styles.line}></div>
-        </div>
-        <div className={styles.line}></div>
-        <div className={styles.statisticItem}>
-          <h2 className={styles.title}>68k</h2>
-          <p className={styles.category}>Happy Customers</p>
-          <p className={styles.description}>Lorem ipsum is simply dummy, adipisicing elit. Delectus modi quibusdam</p>
-          <div className={styles.line}></div>
+        <div className={styles.statisticList2}>
+          <div className={styles.statisticItem}>
+            <h2 className={styles.title}>30+</h2>
+            <p className={styles.category}>Waste Mangements</p>
+            <p className={styles.description}>Lorem ipsum is simply dummy, adipisicing elit. Delectus modi quibusdam</p>
+          </div>
+          <div className={styles.statisticItem}>
+            <h2 className={styles.title}>68k</h2>
+            <p className={styles.category}>Happy Customers</p>
+            <p className={styles.description}>Lorem ipsum is simply dummy, adipisicing elit. Delectus modi quibusdam</p>
+            <div className={styles.line}></div>
+          </div>
         </div>
       </div>
     </section>
