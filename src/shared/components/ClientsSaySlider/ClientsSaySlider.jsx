649e2560cce77ab110c5c8d994161dff1727fe36
Dreamkaman <riabushko@gmail.com>
2022-11-28 02:07:41 +0200

    Next stage custom slider

--- src/shared/components/ClientsSaySlider/ClientsSaySlider.jsx
+++ src/shared/components/ClientsSaySlider/ClientsSaySlider.jsx
@@ -4,13 +4,15 @@ import GetSvg from '../GetSvg/index.js';
 
 const ClientsSaySlider = () => {
   const slidesMarkUp = slides.map((slide) => (
-    <div key={slide.id}>
+    <div className={s.clientPhotoWithTextWrapper} key={slide.id}>
       <div className={s.clientPhotoWrapper}>
         <img className={s.clientPhoto} src={slide.imageURL} alt="Client" />
         <GetSvg name={'icon-quotes'} className={s.iconQuote} />
       </div>
-      <p className={s.clientQuote}>{slide.quote}</p>
-      <p className={s.clientName}>{slide.clientName}</p>
+      <div className={s.clientQuoteWithName}>
+        <p className={s.clientQuote}>{slide.quote}</p>
+        <p className={s.clientName}>{slide.clientName}</p>
+      </div>
     </div>
   ));
   return <div className={s.slider}>{slidesMarkUp}</div>;
