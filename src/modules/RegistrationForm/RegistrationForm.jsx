f2904a2b91a85eab930f85bf7e647af7c38b2254
autudofe <ihor161994@gmail.com>
2022-12-03 11:01:40 +0200

    commit: login signUp

--- src/modules/RegistrationForm/RegistrationForm.jsx
+++ src/modules/RegistrationForm/RegistrationForm.jsx
@@ -1,45 +1,69 @@
 import styles from './RegistrationForm.module.scss';
-import image from '../../images/loginBackground.png';
+import image from '../../images/backgroundForm.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { Field, Form, Formik } from 'formik';
 import classnames from 'classnames';
-import * as yup from 'yup';
+import * as Yup from 'yup';
 
-const validateEmail = (value) => {
-  if (!value) {
-    return 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-    return "Invalid email address. Example: 'example@mail.com'";
-  }
-};
+const validationSchema = Yup.object({
+  company: Yup.string().required('Required'),
+  email: Yup.string().required('Required').email("Invalid email. Example: 'example@mail.com'"),
+  phone: Yup.string()
+    .required('Required')
+    .matches(
+      /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)$/,
+      'Phone number is not valid',
+    ),
+  country: Yup.string().required('Required'),
+  city: Yup.string().required('Required'),
+  street: Yup.string().required('Required'),
+  password: Yup.string()
+    .required('Required')
+    .matches(
+      /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g,
+      "Invalid password. Example:'a0A#ccsxcvx'",
+    ),
+  confirmPassword: Yup.string()
+    .required('Required')
+    .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+});
 
-const validatePassword = (value) => {
-  if (!value) {
-    return 'Required';
-  } else if (
-    !/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g.test(value)
-  ) {
-    return "Invalid password. Example:'a0A#ccsxcvx'";
-  }
-};
+ const ErrorField = (errors, touched) => {
+  return (
+    <div
+      className={classnames(styles.errorMessage, {
+        [styles.errorMessageActive]: errors && touched,
+      })}
+    >
+      {errors}
+    </div>
+  );
+}
 
 const RegistrationForm = () => {
   return (
-    <div className={styles.loginFormContainer}>
+    <div className={styles.formContainer}>
       <div className={styles.headerContainer} style={{ backgroundImage: `url(${image})` }}>
         <FontAwesomeIcon className={styles.closeIcon} icon={faXmark} />
-        <span>Login</span>
+        <span>Sign Up</span>
       </div>
 
       <Formik
         initialValues={{
+          company: '',
           email: '',
-          waste: 'plastic',
-          option: 'free',
+          phone: '',
+          country: '',
+          city: '',
+          street: '',
+          waste: 'Paper',
+          option: 'Free',
           password: '',
+          confirmPassword: '',
           toggle: false,
         }}
+        validationSchema={validationSchema}
         onSubmit={(values) => {
           console.log('submit', values);
         }}
@@ -47,17 +71,33 @@ const RegistrationForm = () => {
         {({ values, errors, touched }) => (
           <Form>
             <div className={styles.fieldsContainer}>
-              <label className={classnames(styles.label)}>Company name</label>
+              <label
+                className={classnames(styles.label, {
+                  [styles.errorLabel]: errors.company && touched.company,
+                })}
+              >
+                Company name
+              </label>
               <Field
-                className={classnames(styles.field)}
+                className={classnames(styles.field, {
+                  [styles.errorField]: errors.company && touched.company,
+                })}
                 name="company"
                 type="text"
                 placeholder="Enter company name"
-                validate={validateEmail}
               />
             </div>
 
-            <div className={classnames(styles.errorMessage)}></div>
+
+            {/*TODO*/}
+            {/*<div
+              className={classnames(styles.errorMessage, {
+                [styles.errorMessageActive]: errors.company && touched.company,
+              })}
+            >
+              {errors.company}
+            </div>*/}
+            {ErrorField(errors.company, touched.company)}
 
             <div className={styles.fieldsContainer}>
               <label
@@ -65,7 +105,7 @@ const RegistrationForm = () => {
                   [styles.errorLabel]: errors.email && touched.email,
                 })}
               >
-                Email:
+                Email
               </label>
               <Field
                 className={classnames(styles.field, {
@@ -74,7 +114,6 @@ const RegistrationForm = () => {
                 name="email"
                 type="email"
                 placeholder="Enter email"
-                validate={validateEmail}
               />
             </div>
 
@@ -87,88 +126,118 @@ const RegistrationForm = () => {
             </div>
 
             <div className={styles.fieldsContainer}>
-              <label className={classnames(styles.label)}>Phone:</label>
+              <label
+                className={classnames(styles.label, {
+                  [styles.errorLabel]: errors.phone && touched.phone,
+                })}
+              >
+                Phone
+              </label>
               <Field
-                className={classnames(styles.field)}
+                className={classnames(styles.field, {
+                  [styles.errorField]: errors.phone && touched.phone,
+                })}
                 name="phone"
-                type="phone"
+                type="text"
                 placeholder="Enter phone"
-                /*validate={validateEmail}*/
               />
             </div>
 
             <div
               className={classnames(styles.errorMessage, {
-                [styles.errorMessageActive]: errors.email && touched.email,
+                [styles.errorMessageActive]: errors.phone && touched.phone,
               })}
             >
-              {errors.email}
+              {errors.phone}
             </div>
 
             <div className={styles.fieldsContainer}>
-              <label className={classnames(styles.label)}>country</label>
+              <label
+                className={classnames(styles.label, {
+                  [styles.errorLabel]: errors.country && touched.country,
+                })}
+              >
+                Country
+              </label>
               <Field
-                className={classnames(styles.field)}
+                className={classnames(styles.field, {
+                  [styles.errorField]: errors.country && touched.country,
+                })}
                 name="country"
                 type="country"
                 placeholder="Enter country"
-                /*validate={validateEmail}*/
               />
             </div>
 
             <div
               className={classnames(styles.errorMessage, {
-                [styles.errorMessageActive]: errors.email && touched.email,
+                [styles.errorMessageActive]: errors.country && touched.country,
               })}
             >
-              {errors.email}
+              {errors.country}
             </div>
 
             <div className={styles.fieldsContainer}>
-              <label className={classnames(styles.label)}>City</label>
+              <label
+                className={classnames(styles.label, {
+                  [styles.errorLabel]: errors.city && touched.city,
+                })}
+              >
+                City
+              </label>
               <Field
-                className={classnames(styles.field)}
+                className={classnames(styles.field, {
+                  [styles.errorField]: errors.city && touched.city,
+                })}
                 name="city"
                 type="city"
                 placeholder="Enter city"
-                /*validate={validateEmail}*/
               />
             </div>
 
             <div
               className={classnames(styles.errorMessage, {
-                [styles.errorMessageActive]: errors.email && touched.email,
+                [styles.errorMessageActive]: errors.city && touched.city,
               })}
             >
-              {errors.email}
+              {errors.city}
             </div>
 
             <div className={styles.fieldsContainer}>
-              <label className={classnames(styles.label)}>Street</label>
+              <label
+                className={classnames(styles.label, {
+                  [styles.errorLabel]: errors.street && touched.street,
+                })}
+              >
+                Street
+              </label>
               <Field
-                className={classnames(styles.field)}
+                className={classnames(styles.field, {
+                  [styles.errorField]: errors.street && touched.street,
+                })}
                 name="street"
                 type="street"
                 placeholder="Enter street"
-                /*validate={validateEmail}*/
               />
             </div>
 
             <div
               className={classnames(styles.errorMessage, {
-                [styles.errorMessageActive]: errors.email && touched.email,
+                [styles.errorMessageActive]: errors.street && touched.street,
               })}
             >
-              {errors.email}
+              {errors.street}
             </div>
 
             <div className={styles.fieldsContainer}>
               <label className={classnames(styles.label)}>Type of waste</label>
 
               <Field name="waste" as="select" className={classnames(styles.field)}>
-                <option value="plastic">plastic</option>
-                <option value="green">Green</option>
-                <option value="blue">Blue</option>
+                <option value="Paper">Paper</option>
+                <option value="Plastic">Plastic</option>
+                <option value="Glass">Glass</option>
+                <option value="Metal">Metal</option>
+                <option value="Electric Supply elements">Electric Supply elements</option>
               </Field>
             </div>
             <div className={classnames(styles.errorMessage)}></div>
@@ -177,8 +246,8 @@ const RegistrationForm = () => {
               <label className={classnames(styles.label)}>Delivery condition option</label>
 
               <Field name="option" as="select" className={classnames(styles.field)}>
-                <option value="free">free</option>
-                <option value="paid">paid</option>
+                <option value="Free">Free</option>
+                <option value="Paid">Paid</option>
               </Field>
             </div>
 
@@ -199,7 +268,6 @@ const RegistrationForm = () => {
                 name="password"
                 type={values.toggle ? 'text' : 'password'}
                 placeholder="Enter password"
-                validate={validatePassword}
               />
             </div>
 
@@ -211,13 +279,37 @@ const RegistrationForm = () => {
               {errors.password}
             </div>
 
+            <div className={styles.fieldsContainer}>
+              <label
+                className={classnames(styles.label, {
+                  [styles.errorLabel]: errors.confirmPassword && touched.confirmPassword,
+                })}
+              >
+                Confirm Password
+              </label>
+              <Field
+                className={classnames(styles.field, {
+                  [styles.errorField]: errors.confirmPassword && touched.confirmPassword,
+                })}
+                name="confirmPassword"
+                type={values.toggle ? 'text' : 'password'}
+                placeholder="Enter password"
+              />
+            </div>
+
+            <div
+              className={classnames(styles.errorMessage, {
+                [styles.errorMessageActive]: errors.confirmPassword && touched.confirmPassword,
+              })}
+            >
+              {errors.confirmPassword}
+            </div>
+
             <div className={styles.toolsContainer}>
               <label>
                 <Field type="checkbox" name="toggle" />
                 Show password
               </label>
-
-              <a href="#">Forgot Password?</a>
             </div>
 
             <button type="submit">Submit</button>
