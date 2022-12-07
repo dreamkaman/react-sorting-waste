d8eecb95207077da997fba02ef694d0188b0221f
autudofe <ihor161994@gmail.com>
2022-12-07 21:04:34 +0200

    commit: our team, routs

---
+++ src/pages/OurTeamPage/dataOurTeam.js
@@ -0,0 +1,55 @@
+import AshkhenGevorgyan from './../../images/our-team/AshkhenGevorgyan.jpg';
+import BabkenSargsyan from './../../images/our-team/BabkenSargsyan.jpg';
+import AnnaSargsyan from './../../images/our-team/AnnaSargsyan.jpg';
+import VolodymyrRiabushko from './../../images/our-team/VolodymyrRiabushko.jpg';
+import TejaswiniYellayyagari from './../../images/our-team/TejaswiniYellayyagari.jpg';
+import AndriiTretiak from './../../images/our-team/AndriiTretiak.jpg';
+import IhorOnishchenko from './../../images/our-team/IhorOnishchenko.jpg';
+
+import { v4 as randomId } from 'uuid';
+
+export const dataPeople = [
+  {
+    id: randomId(),
+    name: 'Ashkhen Gevorgyan',
+    image: AshkhenGevorgyan,
+    role: 'QA Engineer/Copywriter',
+  },
+  {
+    id: randomId(),
+    name: 'Babken Sargsyan',
+    image: BabkenSargsyan,
+    role: 'Back end developer',
+  },
+  {
+    id: randomId(),
+    name: 'Anna Sargsyan',
+    image: AnnaSargsyan,
+    role: 'QA Engineer/Scrum Master',
+  },
+  {
+    id: randomId(),
+    name: 'Volodymyr Riabushko',
+    image: VolodymyrRiabushko,
+    role: 'Front end developer',
+  },
+  {
+    id: randomId(),
+    name: 'Tejaswini Yellayyagari',
+    image: TejaswiniYellayyagari,
+    role: 'Back end developer',
+  },
+  {
+    id: randomId(),
+    name: 'Andrii Tretiak',
+    image: AndriiTretiak,
+    role: 'Front end developer',
+  },
+  {
+    id: randomId(),
+    name: 'Ihor Onishchenko',
+    image: IhorOnishchenko,
+    role: 'Front end developer',
+  },
+
+];
