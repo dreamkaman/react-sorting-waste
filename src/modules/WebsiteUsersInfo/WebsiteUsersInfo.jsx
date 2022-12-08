a5f236671364e5254eec58abf25989f347bd5fcf
Dreamkaman <riabushko@gmail.com>
2022-12-08 20:10:45 +0200

    Added statistic on the home page

--- src/modules/WebsiteUsersInfo/WebsiteUsersInfo.jsx
+++ src/modules/WebsiteUsersInfo/WebsiteUsersInfo.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
-// import { } from 'redux/orders/orderOperations';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { getServicesOperation } from 'redux/services/servicesOperations';
+import { getOrdersOperation } from 'redux/orders/orderOperations';
+import { getFilteredWastePointsOperation } from 'redux/wastePoints/wastePointsOperations';
 
 import { ordersArray } from 'redux/orders/orderSelectors';
 import { ecoServicesArray } from 'redux/services/servicesSelectors';
@@ -9,20 +12,32 @@ import { wastePointsArray } from 'redux/wastePoints/wastePointsSelectors';
 import styles from './WebsiteUsersInfo.module.scss';
 
 const WebsiteUsersInfo = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getServicesOperation());
+    dispatch(getOrdersOperation());
+    dispatch(getFilteredWastePointsOperation());
+  }, [dispatch]);
+
+  const statCustomers = useSelector(ordersArray)?.length;
+  const statEcoServices = useSelector(ecoServicesArray)?.length;
+  const statWastPoints = useSelector(wastePointsArray)?.length;
+
   return (
     <section className={styles.wrap}>
       <div className={styles.statisticList}>
         <div className={styles.statisticList1}>
           <div className={styles.statisticItem}>
-            <p className={styles.numbers}>30+</p>
-            <p className={styles.category}>Customer</p>
+            <p className={styles.numbers}>{statCustomers}</p>
+            <p className={styles.category}>Customers left orders</p>
             <p className={styles.description}>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus modi quibusdam
             </p>
           </div>
           <div className={styles.statisticItem}>
-            <p className={styles.numbers}>30+</p>
-            <p className={styles.category}>Eco Services</p>
+            <p className={styles.numbers}>{statEcoServices}</p>
+            <p className={styles.category}>Eco services registered</p>
             <p className={styles.description}>
               Lorem ipsum is simply dummy, adipisicing elit. Delectus modi quibusdam
             </p>
@@ -30,8 +45,8 @@ const WebsiteUsersInfo = () => {
         </div>
         <div className={styles.statisticList2}>
           <div className={styles.statisticItem}>
-            <p className={styles.numbers}>30+</p>
-            <p className={styles.category}>Waste Mangements</p>
+            <p className={styles.numbers}>{statWastPoints}</p>
+            <p className={styles.category}>Waste collection points</p>
             <p className={styles.description}>
               Lorem ipsum is simply dummy, adipisicing elit. Delectus modi quibusdam
             </p>
