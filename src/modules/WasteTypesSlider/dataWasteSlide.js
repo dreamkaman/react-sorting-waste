c92589ee276875634f6f50abc5865a8f79ac3e62
autudofe <ihor161994@gmail.com>
2022-12-12 02:59:51 +0200

    typography 2

--- src/modules/WasteTypesSlider/dataWasteSlide.js
+++ src/modules/WasteTypesSlider/dataWasteSlide.js
@@ -13,6 +13,7 @@ export const data = [
     title: 'Paper',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ',
+    fullDescription:'',
   },
   {
     id: randomId(),
@@ -20,9 +21,10 @@ export const data = [
     title: 'Plastic',
     description:
       'Right now, 85% of soft plastics used in Australia end up in landfill. We drop these off at collection points so they may be ',
-    /*'Right now, 85% of soft plastics used in Australia end up in landfill. We drop these off at collection points so they may be ' +
+    fullDescription:'Right now, 85% of soft plastics used in Australia end up in landfill. We drop these off at collection points so they may be ' +
       'recycled into fitness trails, outdoor furniture, drinking fountains, bike stands, bollard fences, picket fences, road and ' +
-      'speed humps, rumble bars and more!',*/
+      'speed humps, rumble bars and more!',
+
   },
   {
     id: randomId(),
@@ -30,6 +32,7 @@ export const data = [
     title: 'E-waste',
     description:
       'Globally up to 80% of e-waste is illegally dumped. This is a huge problem as it often leaks large quantities of heavy metals ',
+    fullDescription:'',
     /*'Globally up to 80% of e-waste is illegally dumped. This is a huge problem as it often leaks large quantities of heavy metals' +
       ' into our oceans and waterways. We take it recycling centers where it is then collected to then be processed into new products,' +
       ' such as asphalt, concrete building products, lead, new steel and batteries.'*/
@@ -40,6 +43,7 @@ export const data = [
     title: 'Clothes',
     description:
       'Your clothes will live on as clothes, just in someone else’s wardrobe! We donate wearable clothes and work with partners ',
+    fullDescription:'',
     /*'Your clothes will live on as clothes, just in someone else’s wardrobe! We donate wearable clothes and work with partners to recycle non-wearable clothes.'*/
   },
   {
@@ -48,6 +52,7 @@ export const data = [
     title: 'Batteries',
     description:
       '95% of battery components can be reused and turned into new batteries. We drop them off at a recycling centre, where they get ',
+    fullDescription:'',
     /*'95% of battery components can be reused and turned into new batteries. We drop them off at a recycling centre, where they get' +
       ' picked apart, ready to become new pieces of tech.'*/
   },
